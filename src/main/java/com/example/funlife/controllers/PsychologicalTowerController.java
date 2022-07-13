a9465dfb60d0ea8b131f2db8893e545c4ba155b2
nhuantho <nhuanbc.b19cn488@gmail.com>
2022-07-13 10:40:32 +0700

    fix

--- src/main/java/com/example/funlife/controllers/PsychologicalTowerController.java
+++ src/main/java/com/example/funlife/controllers/PsychologicalTowerController.java
@@ -3,6 +3,7 @@ package com.example.funlife.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.example.funlife.models.PsychologicalTower;
 import com.example.funlife.services.PsychologicalTowerService;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class PsychologicalTowerController {
 	@Autowired 
 	private PsychologicalTowerService psychologicalTowerService;
